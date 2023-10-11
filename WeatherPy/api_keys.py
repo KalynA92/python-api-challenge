f17bd0129956fdcafa5a7c596fdd3397c6f0c266
Kalyn Aguiar <kalynaguiar@hotmail.com>
2023-10-11 03:10:46 -0400

    Deleted my API key info"

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "bd86314455f75a291ef29442576c4548"
+weather_api_key = "YOUR CODE HERE"
 
 # Geoapify API Key
-geoapify_key = "46fcbb7dda0648deba27565799ee5688"
+geoapify_key = "YOUR CODE HERE"
